5fc9b6540cb781e7a9f223d83ff74998ae393755
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-15 14:18:24 -0500

    Adapta el menú principal a RWD usando Bootstrap

--- wp-content/themes/blog-de-viajes/header.php
+++ wp-content/themes/blog-de-viajes/header.php
@@ -10,16 +10,25 @@
       <nav class="navigation">
         <div class="container">
           <div class="row">
-            <div class="col-md-4">
+            <div class="navbar-header">
+              <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" type="button" name="button">
+                <span class="sr-only">Toogle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
               <img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Blog de Viajes" />
             </div>
             <!-- .col-md-4 -->
-            <div class="col-md-8">
+            <div class="navbar-right">
               <?php
                 wp_nav_menu( # Despliega el Menú
                   array(
-                    'theme_location' => 'menu_principal'
-                  )  
+                    'theme_location'  => 'menu_principal',
+                    'container_id'    => 'navbar',                        # Hace referencia al Botón
+                    'container_class' => 'collapse navbar-collapse',      # Agregamos clases de Bootstrap
+                    'menu_class'      => 'nav navbar-nav navbar-right'    # Agregamos clases de Bootstrap
+                  )
                 );
               ?>
             </div>
