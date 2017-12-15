a9063cb6d4a465c26fb6c0cfb850dd05c9df58ed
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-15 11:49:56 -0500

    Implementa 'Bootstrap'

--- wp-content/themes/blog-de-viajes/functions.php
+++ wp-content/themes/blog-de-viajes/functions.php
@@ -2,6 +2,11 @@
 
   /* Agregamos la funcion que nos permitirá hacer uso de una hoja de estilos */
   function blogdeviajes_styles() {
+    # Implementa Bootstrap
+    wp_enqueue_style(
+      'bootstrap-css',
+      'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'
+    );
     # Implementación Normalize
     wp_enqueue_style(
       'normalize',
@@ -11,4 +16,16 @@
     wp_enqueue_style( 'style', get_stylesheet_uri() );        # Por defecto tomará el archivo con el nombre 'style.css' como el archivo principal del sitio
   }
   add_action( 'wp_enqueue_scripts', 'blogdeviajes_styles' );  # Hook
+
+  /* Agregamos la funcion que nos permitirá hacer uso de una hoja de estilos */
+  function blogdeviajes_scripts() {
+    # Implementa librería de jQuery integrada en WordPress
+    wp_enqueue_script( 'jquery' );
+    # Implementa Bootstrap
+    wp_enqueue_script(
+      'bootstrap-js',
+      'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
+    );
+  }
+  add_action( 'wp_enqueue_scripts', 'blogdeviajes_scripts' );  # Hook
 ?>
