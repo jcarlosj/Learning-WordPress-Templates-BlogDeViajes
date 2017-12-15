8148a40d527a99cb5408a0c7f036824dbd2abc8d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-15 12:18:09 -0500

    Crea estructura HTML y Logo para la cabecera del sitio (Usando Bootstrap)

--- wp-content/themes/blog-de-viajes/header.php
+++ wp-content/themes/blog-de-viajes/header.php
@@ -6,3 +6,22 @@
     <?php wp_head(); # Cabecera del sitio (metas, styles, scritps, etc) ?>
   </head>
   <body>
+    <header>
+      <nav class="navigation">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Blog de Viajes" /> 
+            </div>
+            <!-- .col-md-4 -->
+            <div class="col-md-8">
+              Menú
+            </div>
+            <!-- .col-md-8 -->
+          </div>
+          <!-- .row -->
+        </div>
+        <!-- .container -->
+      </nav>
+      <!-- nav.navigation -->
+    </header>
