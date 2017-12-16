f587fbb0868f70eb4bf66dda43c4197cf4a53c88
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-16 10:04:22 -0500

    Crea página principal (front-page.php)

---
+++ wp-content/themes/blog-de-viajes/front-page.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+  <h2 class="text-center">Últimas entradas</h2>
+  <section class="container content">
+    <div class="row">
+      Aquí va en contenido para últimas entradas
+    </div>  <!-- .row -->
+  </section>  <!-- .container .content -->
+<?php get_footer(); ?>
