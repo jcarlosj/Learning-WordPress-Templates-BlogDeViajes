2b4a7548162ed4e248fd2002216ce3d4611cf56a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-17 08:55:28 -0500

    Agrega parámetros al menú de redes sociales para crear la estructura HTML alrededor del mismo

--- wp-content/themes/blog-de-viajes/footer.php
+++ wp-content/themes/blog-de-viajes/footer.php
@@ -14,8 +14,18 @@
             <?php
               wp_nav_menu(
                 array(
-                  'theme_location' => 'menu_redes_sociales'
-                )  
+                  'theme_location'  => 'menu_redes_sociales',
+                  'container'       => 'div',
+                  'container_id'    => 'menu-redes-sociales',
+                  'container_class' => 'menu',
+                  'menu_id'         => 'social-networks',
+                  'menu_class'      => 'menu-items',
+                  'depth'           => 1,       # Profundidad o nivel a imprimir
+                  'link_before'     => '<span class="sr-only">',
+                  'link_after'      => '</span>',
+                  'fallback_cb'     => '',
+                  'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>'       # Lo que va rodear a todos los items de menu
+                )
               );
             ?>
           </div>  <!-- .row -->
