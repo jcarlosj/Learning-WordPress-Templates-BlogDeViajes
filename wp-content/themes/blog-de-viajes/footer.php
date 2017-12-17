af8bfbbdfb152807143d13b8dbdd8bbff9199db5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-17 08:12:18 -0500

    Crea el Footer del sitio

--- wp-content/themes/blog-de-viajes/footer.php
+++ wp-content/themes/blog-de-viajes/footer.php
@@ -1,4 +1,22 @@
+  <footer>
+    <div class="row">
 
+      <div class="col-sm-8">
+        <div class="row">
+          widgets
+        </div>  <!-- .row -->
+      </div>  <!-- .col-sm-8 -->
+
+      <div class="col-sm-4">
+        <div class="row">
+          <h3>Redes Sociales</h3>
+        </div>  <!-- .row -->
+      </div>  <!-- .col-sm-4 -->
+
+    </div>  <!-- .row -->
+
+    <p>Blog de Viajes <?php echo date( 'Y' ); ?> </p>
+  </footer>
   <?php wp_footer(); ?>
   </body>
 </html>
